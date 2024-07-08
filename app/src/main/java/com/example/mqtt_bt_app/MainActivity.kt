46c0e3a11400d0a1282d65d9f65954944da2af55
NIIPP_LBR_BTMN <a.urmanow@mail.ru>
2024-07-08 09:34:18 +0700

    add permission

--- app/src/main/java/com/example/mqtt_bt_app/MainActivity.kt
+++ app/src/main/java/com/example/mqtt_bt_app/MainActivity.kt
@@ -5,8 +5,18 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import org.eclipse.paho.android.service.MqttAndroidClient
+import org.eclipse.paho.client.mqttv3.MqttClient
 
 class MainActivity : AppCompatActivity() {
+    private val mqttAndroidClient: MqttAndroidClient? = null
+    private val serverUri = "tcp://broker.hivemq.com:1883" // Замените на адрес вашего брокера
+    private val clientId = MqttClient.generateClientId()
+    private val subscriptionTopic = "test/topic"
+    private val publishTopic = "test/topic"
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
