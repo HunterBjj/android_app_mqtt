9f43e8f46218614a7ecb6c3f2e926da314ce86ce
HunterBjj <a.urmanow@mail.ru>
2024-07-22 16:01:16 +0700

    Update main.py

--- python_mqtt/main.py
+++ python_mqtt/main.py
@@ -2,20 +2,19 @@ import paho.mqtt.client as mqtt
 from bleak import BleakClient
 import asyncio
 
-# MQTT settings
+# MQTT settings.
 MQTT_BROKER = "localhost"
 MQTT_PORT = 1883
 MQTT_TOPICS = [("mqtt/color", 0), ("mqtt/glimpse", 0)]  
 
-# BLE settings
 BLE_DEVICE_ADDRESS = "11:11:12:12:11:12"  
 BLE_CHARACTERISTIC_UUID = "00002a37-0000-1000-8000-00805f9b34fb"  
 
 client = mqtt.Client()
 
-# Global variables to hold the current color and glimpse values
 current_color = None
 current_glimpse = None
+
 
 async def send_ble_data(data):
     try:
@@ -25,11 +24,13 @@ async def send_ble_data(data):
     except Exception as e:
         print(f"Failed to send data to BLE device: {e}")
 
+
 def on_connect(client, userdata, flags, rc):
     print(f"Connected to MQTT broker with result code {rc}")
     for topic, qos in MQTT_TOPICS:
         client.subscribe((topic, qos))
     print(f"Subscribed to topics: {MQTT_TOPICS}")
+
 
 def on_message(client, userdata, msg):
     global current_color, current_glimpse
@@ -46,16 +47,17 @@ def on_message(client, userdata, msg):
         asyncio.run(send_ble_data(current_color))
         asyncio.run(send_ble_data(current_glimpse))
 
+
 async def handle_ble_notification(sender, data):
     message = data.decode()
     print(f"Received data from BLE device: {message}")
-
     if 'R' or 'Y' or 'G' or 'W' in message:  # Данные которые пришли по BLE. 
         client.publish("mqtt/color", message)
         print(f"Published {message} to mqtt/color")
     elif '0' or '1' or '2' in message:
         client.publish("mqtt/glimpse", message)
         print(f"Published {message} to mqtt/glimpse")
+
 
 async def listen_ble_notifications():
     try:
