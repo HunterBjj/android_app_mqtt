c28e4714f3c2aaba95fec97c825469f10e55840a
HunterBjj <a.urmanow@mail.ru>
2024-07-25 15:42:41 +0700

    Update main.py

--- python_mqtt/main.py
+++ python_mqtt/main.py
@@ -25,6 +25,8 @@ class BLEServer:
             self.service = MyService(self.peripheral)
         except ValueError as e:
             print(f"Error initializing MyService: {e}")
+        self.peripheral.addService(self.service)  # Добавление сервиса к периферии
+        self.advertise_name = "BLEDevice_MQTT"  # Определяем атрибут advertise_name
             
     def advertise(self, name):
         print(f'Advertising BLE device with name: {name}')
